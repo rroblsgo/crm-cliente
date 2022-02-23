2b016f7b73579f161498a8e006cecdf4e0b1dbf9
Ricardo Robles <rroblesgo@gmail.com>
2022-02-23 23:59:11 +0100

    Ajustes Finales para gestion local y remota

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -28,7 +28,7 @@ const Formulario = ({ cliente, cargando }) => {
       let respuesta;
       if (cliente.id) {
         // editando un registro
-        const url = `http://localhost:4000/clientes/${cliente.id}`;
+        const url = `${import.meta.env.VITE_API_URL}/${cliente.id}`;
         respuesta = await fetch(url, {
           method: 'PUT',
           body: JSON.stringify(values),
@@ -38,7 +38,7 @@ const Formulario = ({ cliente, cargando }) => {
         });
       } else {
         // nuevo registro
-        const url = 'http://localhost:4000/clientes';
+        const url = import.meta.env.VITE_API_URL;
         respuesta = await fetch(url, {
           method: 'POST',
           body: JSON.stringify(values),
